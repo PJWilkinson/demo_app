c868a0b433b2bc3000ef72adbf62caa2f45b5dee
Philip Wilkinson <philip@mysticinternet.com>
2013-03-18 16:58:08 +0000

    First time Git upload

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '0a8e58bfef14405212ace68de6eaa8e9b6be3bbf5ba62a6a603dc6c2d9ef78c4d32cf73015d238f35ce5eb84825d3aed0d109ee13fab636d39ee32562dfa7292'
